2d67bb70874cabc2895096049c7c58538d5456bb
Burhan Yılmaz <burhan.yilmaz@consultant.turkcell.com.tr>
2023-02-20 11:53:25 +0300

    search

--- pages/search.js
+++ pages/search.js
@@ -3,12 +3,10 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import { Layout } from "../layout";
 import styles from "../assets/styles/Search.module.scss";
-import { Search, Icon, Card, Breadcrumb } from "../components";
+import { Search, Icon, Card, Breadcrumb, CardLink } from "../components";
 import slug from 'slug'
 
-import { navlist } from '../utils/Nav';
-
-export default function SearchPage() {
+export default function SearchPage({navlist, statics}) {
   const [data, setData] = useState();
   const router = useRouter();
 
@@ -29,22 +27,10 @@ export default function SearchPage() {
   }, [router.query.keyword])
 
   const blogDetailUrl = '/blog-detay';
-  const sectorDetailUrl = '/sektor';
-  const systemDetailUrl = '/sistem';
-  const systemsFolder = '/sistemler';
-  const sectorsFolder = '/sektorler';
+  const sectorDetailUrl = '/sektorler';
   const corporateFolder = '/kurumsal';
-  const servicesFolder = '/hizmetlerimiz';
-
-
-  const total = (data?.blogs?.length || 0) + (data?.corporate?.length || 0) + (data?.sector_contents?.length || 0) + (data?.sectors?.length || 0) + (data?.services?.length || 0) + (data?.system_contents?.length || 0) + (data?.systems?.length || 0)
-
-  const cards = [
-    { title: 'B-400', subTitle: "Katlanır Kasa", image: '/images/cards/card-2.jpg' },
-    { title: 'B-400', subTitle: "Katlanır Kasa", image: '/images/cards/card-2.jpg' },
-    { title: 'B-400', subTitle: "Katlanır Kasa", image: '/images/cards/card-2.jpg' },
-    { title: 'B-400', subTitle: "Katlanır Kasa", image: '/images/cards/card-2.jpg' },
-  ]
+  const productDetailUrl = '/urun-detay';
+  const productGroupUrl = '/urun';
 
   const breadcrumbList = [
     {
@@ -52,105 +38,58 @@ export default function SearchPage() {
       href: '/'
     },
     {
-      title: 'Kurumsal',
-      href: '/'
-    },
-    {
-      title: 'Search'
+      title: 'Arama'
     }
   ]
 
   return (
     <>
-      <Layout navlist={navlist}>
+      <Layout navlist={navlist} statics={statics}>
         <section className={styles["search"]}>
         <Breadcrumb data={breadcrumbList} className={'breadcrumb'} />
 
           <h2>Arama Sonuçları</h2>
-          <h4><b>“KASA” </b>ile ilgili 22 sonuç bulundu.</h4>
+          <h4><b>“{router.query.keyword}” </b>ile ilgili 22 sonuç bulundu.</h4>
           <Search className={styles['search-input']} />
 
-          <div className={styles["content"]}>
-            <h3>Sürdürülebilirlik</h3>
-            <div className={styles["text"]}>
-              https://www.benoplast.com/b-400-katlanir-kasa-30514684
-            </div>
-
-            <Link href={`#`} className={styles['button']}><Icon icon='arrow' /></Link>
-          </div>
-
-          <div className={styles["content"]}>
-            <h3>Sürdürülebilirlik</h3>
-            <div className={styles["text"]}>
-              https://www.benoplast.com/b-400-katlanir-kasa-30514684
-            </div>
-
-            <Link href={`#`} className={styles['button']}><Icon icon='arrow' /></Link>
-          </div>
-
-          <div className={styles['search__cards']}>
-            <h4>Pasif Kilitli Katlanır Kasalar</h4>
-            <div>
-              {cards.map((item, index) => <Card key={index} title={item.title} subTitle={item.subTitle} image={item.image} path={`${'#'}`} />)}
-            </div>
-          </div>
-
           {data && <>
-            {data?.blogs?.map((item, index) => <div className={styles["item"]} key={index}>
-              <div className={styles["content"]}>
+            {data?.blogs?.map((item, index) => 
+              <div className={styles["content"]} key={index}>
                 <h3>{item.title}</h3>
-                <div className={styles["text"]} dangerouslySetInnerHTML={{ __html: item.content }} />
+                <div className={styles["text"]}>{process.env.HOST_URL}{blogDetailUrl}/{slug(item.title)}-{item.id}-{item.cat_id}</div>
+                <Link href={`${blogDetailUrl}/${slug(item.title)}-${item.id}-${item.cat_id}`} className={styles['button']}><Icon icon='arrow' /></Link>
               </div>
-              <Link href={`${blogDetailUrl}/${slug(item.title)}-${item.id}-${item.cat_id}`}>Sayfaya Git <Icon icon='arrow' /></Link>
-            </div>)}
+            )}
 
-            {data?.corporate?.map((item, index) => <div className={styles["item"]} key={index}>
-              <div className={styles["content"]}>
+            {data?.corporate?.map((item, index) => 
+              <div className={styles["content"]} key={index}>
                 <h3>{item.title}</h3>
-                <div className={styles["text"]} dangerouslySetInnerHTML={{ __html: item.content }} />
+                <div className={styles["text"]}>{process.env.HOST_URL}{corporateFolder}/{slug(item.title)}-{item.id}</div>
+                <Link href={`${corporateFolder}/${slug(item.title)}-${item.id}`} className={styles['button']}><Icon icon='arrow' /></Link>
               </div>
-              <Link href={`${corporateFolder}/${slug(item.title)}-${item.id}`}>Sayfaya Git <Icon icon='arrow' /></Link>
-            </div>)}
+            )}
 
-            {data?.sector_contents?.map((item, index) => <div className={styles["item"]} key={index}>
-              <div className={styles["content"]}>
+            {data?.sectors?.map((item, index) => 
+              <div className={styles["content"]} key={index}>
                 <h3>{item.title}</h3>
-                <div className={styles["text"]} dangerouslySetInnerHTML={{ __html: item.content }} />
+                <div className={styles["text"]}>{process.env.HOST_URL}{sectorDetailUrl}/{slug(item.title)}-{item.id}</div>
+                <Link href={`${sectorDetailUrl}/${slug(item.title)}-${item.id}`} className={styles['button']}><Icon icon='arrow' /></Link>
               </div>
-              <Link href={`${sectorDetailUrl}/${slug(item.title)}-${item.id}-${item.cat_id}`}>Sayfaya Git <Icon icon='arrow' /></Link>
-            </div>)}
+            )}
 
-            {data?.sectors?.map((item, index) => <div className={styles["item"]} key={index}>
-              <div className={styles["content"]}>
-                <h3>{item.title}</h3>
-                <div className={styles["text"]} dangerouslySetInnerHTML={{ __html: item.content }} />
+            {data?.prodcuts?.length > 0 && <div className={styles['search__cards']}>
+              <h4>Ürünler</h4>
+              <div>
+                {data?.prodcuts?.map((item, index) => <Card key={index} title={item.title} image={item.listing_image} path={`${productDetailUrl}/${slug(item.title)}-${item.id}-${item.group_id}`} />)}
               </div>
-              <Link href={`${sectorsFolder}/${slug(item.title)}-${item.id}`}>Sayfaya Git <Icon icon='arrow' /></Link>
-            </div>)}
+            </div>}
 
-            {data?.services?.map((item, index) => <div className={styles["item"]} key={index}>
-              <div className={styles["content"]}>
-                <h3>{item.title}</h3>
-                <div className={styles["text"]} dangerouslySetInnerHTML={{ __html: item.content }} />
+            {data?.prodcutgroups?.length > 0 && <div className={styles['search__cards']}>
+              <h4>Ürün Grupları</h4>
+              <div>
+                {data?.prodcutgroups?.map((item, index) => <span key={index}><CardLink title={item?.title} image={item?.listing_image} path={`${productGroupUrl}/${slug(item.title)}-${item.id}`} /></span>)}
               </div>
-              <Link href={`${servicesFolder}/${slug(item.title)}-${item.id}`}>Sayfaya Git <Icon icon='arrow' /></Link>
-            </div>)}
-
-            {data?.system_contents?.map((item, index) => <div className={styles["item"]} key={index}>
-              <div className={styles["content"]}>
-                <h3>{item.title}</h3>
-                <div className={styles["text"]} dangerouslySetInnerHTML={{ __html: item.content }} />
-              </div>
-              <Link href={`${systemDetailUrl}/${slug(item.title)}-${item.id}-${item.cat_id}`}>Sayfaya Git <Icon icon='arrow' /></Link>
-            </div>)}
-
-            {data?.systems?.map((item, index) => <div className={styles["item"]} key={index}>
-              <div className={styles["content"]}>
-                <h3>{item.title}</h3>
-                <div className={styles["text"]} dangerouslySetInnerHTML={{ __html: item.content }} />
-              </div>
-              <Link href={`${systemsFolder}/${slug(item.title)}-${item.id}`}>Sayfaya Git <Icon icon='arrow' /></Link>
-            </div>)}
+            </div>}
 
           </>}
         </section>
@@ -159,22 +98,23 @@ export default function SearchPage() {
   );
 }
 
-// export async function getStaticProps() {
-//   const options = {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({ language: 'tr' })
-//   }
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ language: 'tr' })
+  }
 
-//   const navlist = await fetch(`${process.env.API_URL}/navi`, options).then(r => r.json()).then(data => data.Result);
+  const navlist = await fetch(`${process.env.API_URL}/navi`, options).then(r => r.json()).then(data => data.Result);
+  const statics = await fetch(`${process.env.API_URL}/statics`, options).then(r => r.json()).then(data => data.Result);
 
-
-//   return {
-//     props: {
-//       navlist
-//     },
-//     revalidate: 10,
-//   }
-// }
+  return {
+    props: {
+      navlist,
+      statics,
+    },
+    revalidate: 10,
+  }
+}
